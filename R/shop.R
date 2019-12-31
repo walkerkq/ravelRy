5819dfeaca4a168d0917447caf02859d0e2f1802
walkerkq <walkerkq2@gmail.com>
2019-12-31 09:29:34 -0600

    add shop functions

---
+++ R/shop.R
@@ -0,0 +1,65 @@
+#' Search shops
+#'
+#' This function retrieves shops based on search terms.
+#'
+#' @param query search string
+#' @param shop_type_id set to 1 to restrict to local stores only (filter out chains and online stores)
+#' @param lat latitude for geographic search
+#' @param lng longitude for geographic search
+#' @param radius radius for geographic search
+#' @param units units for geographic search, `miles` or `km`
+#' @param page result page to retrieve; defaults to 1
+#' @param page_size number of results to retrieve; defaults to 100
+#' @param sort options: `best`, `added`, `favorites`
+#'
+#' @return tibble
+#'
+#' @examples search_shops(lat = 42.35, lng = -71.05, radius = 100)
+#'
+#' @export
+#'
+search_shops <- function(query = NULL,
+                         shop_type_id = NULL,
+                         lat = NULL,
+                         lng = NULL,
+                         radius = NULL,
+                         units = NULL,
+                         page = NULL,
+                         page_size = NULL,
+                         sort = NULL){
+
+  response <- ravelry_get(path = '/shops/search.json',
+                          query = list(query = query,
+                                       shop_type_id = shop_type_id,
+                                       lat = lat,
+                                       lng = lng,
+                                       radius = radius,
+                                       units = units,
+                                       page = page,
+                                       page_size = page_size,
+                                       sort = sort))
+
+  fromJSONtoTibble(response)
+
+}
+
+#' Get details for a shop
+#'
+#' This function retrieves details for a shop using its id.
+#'
+#' @param id shop id
+#'
+#' @return tibble
+#'
+#' @export
+#'
+get_shop <- function(id){
+
+  response <- ravelry_get(path = paste0('/shops/', id, '.json'))
+
+  # this is broken
+  #fromJSONtoTibble(response)
+  response
+
+}
+
